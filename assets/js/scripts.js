016d529c483e13d5f9041e60ff19fd17405229e2
lomejor77 <lomejor77@gmail.com>
2024-07-08 21:15:48 -0400

    implementado jquery cambio de color de texto, toggle para esconder el texto en card y alert en boton

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
     $('#carouselExample').carousel({
         interval: 3000 // Cambia el valor para ajustar la velocidad del carrusel
     });
+
+    $('#enviarCorreo').click(function() {
+        alert('El correo fue enviado correctamente...');
+    });
+
+    $("h4:contains('INGREDIENTES')").on('dblclick', function() {
+        $(this).toggleClass('red-text');
+    });
+       
+    $("h4:contains('PREPARACIÓN')").on('dblclick', function() {
+        $(this).toggleClass('red-text');
+    });
+
+    $(".card-title").click(function() {
+        $(this).next(".card-text").toggle();
+    });
+
 });
+
